671a657c4295b086468d875f7c3cfe0077f6ff2c
RunningLiLi <3238367573@qq.com>
2023-06-03 22:01:37 +0800

    feat: tailwind

--- packages/vite_css/src/App.tsx
+++ packages/vite_css/src/App.tsx
@@ -1,23 +1,38 @@
-import "./index.module.scss"
+import "./index.module.scss";
 // import styles from "./index.module.css"
-import styled from 'styled-components';
-import { css } from '@emotion/css'
-function App() {
+import styled from "styled-components";
+import { css } from "@emotion/css";
 
-  const color = 'white'
+function App() {
+  const color = "white";
   const Wrapper = styled.section`
-  padding: 4em;
-  background: papayawhip;
-`;
-  return <Wrapper><h1 className={css`
-      padding: 32px;
-      background-color: hotpink;
-      font-size: 24px;
-      border-radius: 4px;
-      &:hover {
-        color: ${color};
-      }
-    `}>hello world</h1></Wrapper>
+    padding: 4em;
+    background: papayawhip;
+  `;
+  return (
+    <Wrapper>
+      <h1
+        className={css`
+          padding: 32px;
+          background-color: hotpink;
+          font-size: 24px;
+          border-radius: 4px;
+          &:hover {
+            color: ${color};
+          }
+        `}
+      >
+        hello world
+      </h1>
+      <h2
+        // bg="blue-400 hover:blue-500 dark:blue-500 dark:hover:blue-600"
+        className="font-bold text-2xl mb-2"
+      >
+        hello windicss
+      </h2>
+      <h2 className="bg-amber-200">hello tailwind</h2>
+    </Wrapper>
+  );
 }
 
-export default App
+export default App;
--- packages/vite_css/vite.config.ts
+++ packages/vite_css/vite.config.ts
@@ -1,26 +1,34 @@
-import {defineConfig, normalizePath} from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path"
-import autoprefixer from 'autoprefixer';
+import { defineConfig, normalizePath } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import autoprefixer from "autoprefixer";
+import windi from "vite-plugin-windicss";
+import tailwindcss from "tailwindcss";
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
-  css:{
-    preprocessorOptions:{
-      scss:{
-        additionalData: `@import "${normalizePath(path.resolve(__dirname,"./src/variable.scss"))}";`
-      }
+  plugins: [react(), windi()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "${normalizePath(
+          path.resolve(__dirname, "./src/variable.scss")
+        )}";`,
+      },
     },
     modules: {
       // 一般我们可以通过 generateScopedName 属性来对生成的类名进行自定义
       // 其中，name 表示当前文件名，local 表示类名
-      generateScopedName: "[name]__[local]__[hash:base64:5]"
+      generateScopedName: "[name]__[local]__[hash:base64:5]",
     },
-    postcss:{
-      plugins:[autoprefixer({
-        // 指定目标浏览器
-        overrideBrowserslist: ["> 1%","last 2 versions"]
-      })]
+    postcss: {
+      plugins: [
+        tailwindcss(),
+        autoprefixer({
+          // 指定目标浏览器
+          overrideBrowserslist: ["> 1%", "last 2 versions"],
+        }),
+      ],
     },
-  }
-})
+  },
+});
