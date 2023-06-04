bad12e24f9487ac3b345cd29befcf9da23538689
RunningLiLi <3238367573@qq.com>
2023-06-04 13:11:13 +0800

    feat: vite_lint

---
+++ packages/vite_lint/vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import viteEslint from "vite-plugin-eslint";
+import viteStylelint from "vite-plugin-stylelint";
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [react(), viteEslint(), viteStylelint()]
+});
