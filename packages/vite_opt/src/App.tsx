8a78b92c34ae664c8ffadf096b617817610b0ea6
RunningLiLi <3238367573@qq.com>
2023-06-06 00:21:01 +0800

    feat: esbuild_plugin_cdn

--- packages/vite_opt/src/App.tsx
+++ packages/vite_opt/src/App.tsx
@@ -33,7 +33,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">test</p>
+      <p className="read-the-docs">test2</p>
     </>
   );
 }
